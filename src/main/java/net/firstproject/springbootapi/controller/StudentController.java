04f6381731429080e711204ad82001ade63a0036
Daniel <lezniak.daniel@gmail.com>
2024-10-23 20:14:00 +0200

    get parametrs uri

--- src/main/java/net/firstproject/springbootapi/controller/StudentController.java
+++ src/main/java/net/firstproject/springbootapi/controller/StudentController.java
@@ -2,7 +2,11 @@ package net.firstproject.springbootapi.controller;
 
 import net.firstproject.springbootapi.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -14,5 +18,31 @@ public class StudentController {
                 "Dan",
                 "Lez"
         );
+    }
+    @GetMapping("students")
+    private List<Student> getStudents(){
+        ArrayList<Student> array = new ArrayList<>();
+
+        for (int i = 0;i <10;i++){
+            array.add(new Student(
+                    i,
+                    "Dan "+i,
+                    "Lez"+i
+            ));
+        }
+        return array;
+    }
+
+    @GetMapping("students/{id}")
+    public Student studentPathVariable(@PathVariable("id") int id){
+        return new Student(id,"dan","lez");
+    }
+
+    @GetMapping("students/{id}/{firstName}/{lastName}")
+    public Student studentPathVariableFull(@PathVariable("id") int id,
+                                           @PathVariable("firstName") String firstName,
+                                           @PathVariable("lastName") String lastName
+    ){
+        return new Student(id,firstName,lastName);
     }
 }
