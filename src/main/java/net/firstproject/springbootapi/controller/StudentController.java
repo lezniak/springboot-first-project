169a1d1b589e75dd18c3a49b9246237d976d12eb
Daniel <lezniak.daniel@gmail.com>
2024-10-25 14:04:39 +0200

    base url fix slash

--- src/main/java/net/firstproject/springbootapi/controller/StudentController.java
+++ src/main/java/net/firstproject/springbootapi/controller/StudentController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("students/")
+@RequestMapping("students")
 public class StudentController {
 
     @GetMapping("student")
